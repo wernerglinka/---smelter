5adf2f9c984b411beac7f8429281c58d85f9aa6e
Werner Glinka <werner@glinka.co>
2025-02-27 11:26:45 -0800

    added types json to templates

--- src/renderer/src/components/Templates/index.jsx
+++ src/renderer/src/components/Templates/index.jsx
@@ -1,10 +1,17 @@
 import React, { memo } from 'react';
 import { templates } from '@src/templates';
+import templateTypes from '@src/templates/types.json';
 import './styles.css';
 
 const Templates = memo(() => {
+  // Initialize groupedTemplates using types from JSON
+  const groupedTemplates = templateTypes.reduce((acc, type) => {
+    acc[type] = [];
+    return acc;
+  }, {});
+
   // Group templates by type
-  const groupedTemplates = Object.entries(templates).reduce((acc, [key, value]) => {
+  Object.entries(templates).reduce((acc, [key, value]) => {
     if (key.startsWith('page')) {
       acc.pages.push({ id: key, url: `/pages/${key}.js`, label: key.replace('page', '').toUpperCase() });
     } else if (key.endsWith('Section')) {
@@ -13,46 +20,24 @@ const Templates = memo(() => {
       acc.blocks.push({ id: key, url: `/blocks/${key}.js`, label: key.toUpperCase() });
     }
     return acc;
-  }, { pages: [], sections: [], blocks: [] });
+  }, groupedTemplates);
 
   return (
     <div className="templates-wrapper">
-      <h3 className="section-header">Page Templates</h3>
-      {groupedTemplates.pages.map((template, index) => (
-        <div
-          key={template.id}
-          id={`template-page-${index + 1}`}
-          className="template-selection draggable"
-          draggable="true"
-          data-url={template.url}
-        >
-          {template.label}
-        </div>
-      ))}
-
-      <h3 className="section-header">Section Templates</h3>
-      {groupedTemplates.sections.map((template, index) => (
-        <div
-          key={template.id}
-          id={`template-section-${index}`}
-          className="template-selection draggable"
-          draggable="true"
-          data-url={template.url}
-        >
-          {template.label}
-        </div>
-      ))}
-
-      <h3 className="section-header">Block Templates</h3>
-      {groupedTemplates.blocks.map((template, index) => (
-        <div
-          key={template.id}
-          id={`template-block-${index}`}
-          className="template-selection draggable"
-          draggable="true"
-          data-url={template.url}
-        >
-          {template.label}
+      {templateTypes.map(type => (
+        <div key={type}>
+          <h3 className="section-header">{`${type.charAt(0).toUpperCase() + type.slice(1)} Templates`}</h3>
+          {groupedTemplates[type].map((template, index) => (
+            <div
+              key={template.id}
+              id={`template-${type}-${index}`}
+              className="template-selection draggable"
+              draggable="true"
+              data-url={template.url}
+            >
+              {template.label}
+            </div>
+          ))}
         </div>
       ))}
     </div>
